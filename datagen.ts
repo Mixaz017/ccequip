0fa3b14eefdceb1898caab05b3b419205b3f8e0f
Mixaz <31244102+Mixaz017@users.noreply.github.com>
2025-10-27 16:54:05 +0900

    style: fix indents

--- datagen.ts
+++ datagen.ts
@@ -4,13 +4,13 @@ const gamePath = Deno.args[0];
 if (!gamePath) throw Error("No path specified");
 
 type Item = {
-  type: string;
+	type: string;
 };
 
 const items: Item[] = JSON.parse(
-  Deno.readTextFileSync(join(gamePath, "assets", "data", "item-database.json")),
+	Deno.readTextFileSync(join(gamePath, "assets", "data", "item-database.json")),
 ).items;
 Deno.writeTextFileSync(
-  join(Deno.cwd(), "data", "equips.json"),
-  JSON.stringify(items.filter((item) => item.type === "EQUIP")),
+	join(Deno.cwd(), "data", "equips.json"),
+	JSON.stringify(items.filter((item) => item.type === "EQUIP")),
 );
